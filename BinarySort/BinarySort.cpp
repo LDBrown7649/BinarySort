c4209bb2391c0cf7d8f6ea40e79427c2efc1719a
LDBrown7649 <ldbrown7649@gmail.com>
2022-05-04 14:22:47 +1000

    Added comments and prints only the sorted list.

--- BinarySort/BinarySort.cpp
+++ BinarySort/BinarySort.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
 
 int* Sort(int arr[], int numItems) {
+    // Creates a variable tracking if the items were switched in the current pass.
     bool swappedVals = true;
     while (swappedVals) {
         swappedVals = false;
         for (int i = 0; i < numItems - 1; i++) {
+            // Swaps the values if the left value was larger than the right value.
             if (arr[i] > arr[i + 1]) {
                 swappedVals = true;
                 int temp = arr[i + 1];
@@ -12,11 +14,8 @@ int* Sort(int arr[], int numItems) {
                 arr[i] = temp;
             }
         }
-        for (int i = 0; i < numItems; i++) {
-            std::cout << arr[i] << ", ";
-        }
-        std::cout << std::endl;
     }
+    // Returns the now sorted list.
     return arr;
 }
 
@@ -25,6 +24,13 @@ int main()
     //Stores the number of items in the array in a const.
     const int array_size = 25;
     int arr[array_size] = { 14,65,63,1,54,89,84,9,98,57,71,18,21,84,69,28,11,83,13,42,64,58,78,82,13};
+
+    // Sorts the array in ascending order.
     int* sortedArray = Sort(arr, array_size);
-    
+
+    // Displays the array to the console for the user to see.
+    for (int i = 0; i < array_size; i++) {
+        std::cout << sortedArray[i] << ", ";
+    }
+    std::cout << std::endl;
 }
